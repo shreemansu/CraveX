98a1e90b094a07421eaa1829983b7ec76eb1516f
Shreemansu <shreemansusekhar45@gmail.com>
2026-06-07 09:07:41 +0530

    first commit

---
+++ src/main/java/com/carve/cravex/CraveXApplication.java
@@ -0,0 +1,13 @@
+package com.carve.cravex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraveXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraveXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carve/cravex/CraveXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carve.cravex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraveXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
